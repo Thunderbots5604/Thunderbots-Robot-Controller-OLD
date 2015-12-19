001554e3007e964b98a64aba6cf5d8122038992a
Zach Ohara <zjohara@gmail.com>
2015-12-19 14:37:22 -0800

    Remove verbose log statements

--- FtcRobotController/src/main/java/io/github/thunderbots/robotcontroller/fileloader/ThunderOpModeRegister.java
+++ FtcRobotController/src/main/java/io/github/thunderbots/robotcontroller/fileloader/ThunderOpModeRegister.java
@@ -10,30 +10,20 @@ import io.github.thunderbots.robotcontroller.logging.ThunderLog;
 
 public class ThunderOpModeRegister {
 
-    /**
-     * The Op Mode Manager will call this method when it wants a list of all
-     * available op modes. Add your op mode to the list to enable it.
-     *
-     * @param manager op mode manager
-     */
     public static void register(OpModeManager manager) {
-
-        ThunderLog.i("About to register op modes...");
+        ThunderLog.v("About to register op modes...");
         try {
             List<File> fileList = OpModeClassLoader.getFileSet();
-            ThunderLog.d("Preliminary fileList: " + fileList);
             DalvikConverter.getJarList(fileList);
-            ThunderLog.d("Jar-only fileList: " + fileList);
             DalvikConverter.convertJars(fileList);
-            ThunderLog.d("Converted fileList: " + fileList);
             List<Class<? extends OpMode>> opmodeList = OpModeClassLoader.loadJars(fileList);
-            ThunderLog.d("Final opmodeList: " + opmodeList);
-            ThunderLog.i("Now registering OpModes...");
+            ThunderLog.d("Final op mode list: " + opmodeList);
+            ThunderLog.v("Now registering OpModes...");
             for (Class<? extends OpMode> opmode : opmodeList) {
                 if (AnnotationReader.isActive(opmode)) {
                     try {
                         manager.register(AnnotationReader.getOpModeName(opmode), opmode);
-                        ThunderLog.i("Registered " + opmode.getSimpleName());
+                        ThunderLog.v("Registered " + opmode.getSimpleName());
                     } catch (Throwable ex) {
                         ThunderLog.e("Error registering op mode: " + opmode.getSimpleName());
                         ThunderLog.e(ex.getMessage());
