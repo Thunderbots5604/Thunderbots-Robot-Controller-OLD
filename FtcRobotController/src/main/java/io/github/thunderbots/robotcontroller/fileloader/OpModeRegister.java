9e621df9c3410ea7d1c35ea5238e9e4704adc346
Zach Ohara <zjohara@gmail.com>
2015-12-19 14:44:17 -0800

    Remove pointless try/catch structure

--- FtcRobotController/src/main/java/io/github/thunderbots/robotcontroller/fileloader/OpModeRegister.java
+++ FtcRobotController/src/main/java/io/github/thunderbots/robotcontroller/fileloader/OpModeRegister.java
@@ -12,28 +12,21 @@ public class OpModeRegister {
 
     public static void register(OpModeManager manager) {
         ThunderLog.v("About to register op modes...");
-        try {
-            List<File> fileList = OpModeClassLoader.getFileSet();
-            DalvikConverter.getJarList(fileList);
-            DalvikConverter.convertJars(fileList);
-            List<Class<? extends OpMode>> opmodeList = OpModeClassLoader.loadJars(fileList);
-            ThunderLog.d("Final op mode list: " + opmodeList);
-            ThunderLog.v("Now registering OpModes...");
-            for (Class<? extends OpMode> opmode : opmodeList) {
-                if (AnnotationReader.isActive(opmode)) {
-                    try {
-                        manager.register(AnnotationReader.getOpModeName(opmode), opmode);
-                        ThunderLog.v("Registered " + opmode.getSimpleName());
-                    } catch (Throwable ex) {
-                        ThunderLog.e("Error registering op mode: " + opmode.getSimpleName());
-                        ThunderLog.e(ex.getMessage());
-                    }
+        List<File> fileList = OpModeClassLoader.getFileSet();
+        DalvikConverter.getJarList(fileList);
+        DalvikConverter.convertJars(fileList);
+        List<Class<? extends OpMode>> opmodeList = OpModeClassLoader.loadJars(fileList);
+        ThunderLog.d("Final op mode list: " + opmodeList);
+        ThunderLog.v("Now registering OpModes...");
+        for (Class<? extends OpMode> opmode : opmodeList) {
+            if (AnnotationReader.isActive(opmode)) {
+                try {
+                    manager.register(AnnotationReader.getOpModeName(opmode), opmode);
+                    ThunderLog.v("Registered " + opmode.getSimpleName());
+                } catch (Throwable ex) {
+                    ThunderLog.e("Error registering op mode: " + opmode.getSimpleName());
+                    ThunderLog.e(ex.getMessage());
                 }
-            }
-        } catch (Throwable ex) {
-            ThunderLog.e("Error reading external files:");
-            if (ex instanceof Exception) {
-                ex.printStackTrace();
             }
         }
     }
