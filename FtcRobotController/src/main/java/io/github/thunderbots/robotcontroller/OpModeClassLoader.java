2dfff2a14b412fb5c467224410536d272a8f9fb2
Zach Ohara <zjohara@gmail.com>
2015-12-29 16:12:45 -0800

    Reorganize OpModeClassLoader

--- FtcRobotController/src/main/java/io/github/thunderbots/robotcontroller/OpModeClassLoader.java
+++ FtcRobotController/src/main/java/io/github/thunderbots/robotcontroller/OpModeClassLoader.java
@@ -1,17 +1,12 @@
 package io.github.thunderbots.robotcontroller;
 
-import android.os.Environment;
-
-import com.qualcomm.ftcrobotcontroller.FtcRobotControllerActivity;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
-import java.util.LinkedList;
 import java.util.List;
 
 import dalvik.system.DexClassLoader;
@@ -21,117 +16,81 @@ import io.github.thunderbots.robotcontroller.logging.ThunderLog;
 public class OpModeClassLoader {
 
     private ClassLoader classLoader;
+    private List<File> fileList;
     private List<Class<? extends OpMode>> opModeList;
 
-    public List<Class<? extends OpMode>> loadJars(List<File> fileList) {
-        // TODO: call initializeClassLoader before this method
-
-        this.opModeList = new ArrayList<Class<? extends OpMode>>();
-        for (File f : fileList) {
-            loadJarFile(f);
-        }
-
-
-        URL[] jarURLs = getJarURLs(fileList);
-        classLoader = getClassLoader(jarURLs);
-        Thread.currentThread().setContextClassLoader(classLoader);
-        opModeList = new ArrayList<Class<? extends OpMode>>();
-        for (File jarfile : fileList) {
-            if (jarfile.getName().endsWith(".jar")) {
-                try {
-                    loadJarFile(jarfile);
-                } catch (Exception ignore) {}
-            }
-        }
-        return opModeList;
-    }
-
-    private void initializeClassLoader(List<File> fileList) {
-        List<URL> jarList = FileLoader.getUrlList(fileList);
+    public OpModeClassLoader(List<File> fileList) {
+        this.fileList = fileList;
+        List<URL> jarList = FileLoader.getUrlList(this.fileList);
         this.classLoader = getClassLoader(jarList);
         Thread.currentThread().setContextClassLoader(this.classLoader);
     }
 
+    public List<Class<? extends OpMode>> getOpModes() {
+        this.opModeList = new ArrayList<Class<? extends OpMode>>();
+        for (File f : this.fileList) {
+            try {
+                this.loadJar(f);
+            } catch (IOException e) {
+                ThunderLog.e(f.getAbsolutePath() + " cannot be opened");
+                e.printStackTrace();
+            }
+        }
+        return this.opModeList;
+    }
+
+    private void loadJar(File jarFile) throws IOException {
+        File cache = new File(FileLoader.getCacheDirectory() + "/temp/");
+        DexFile jar = DexFile.loadDex(jarFile.getAbsolutePath(), cache.getAbsolutePath(), 0);
+        Enumeration<String> entries = jar.entries();
+        while(entries.hasMoreElements()) {
+            String entry  = entries.nextElement();
+            try {
+                Class<?> c = this.classLoader.loadClass(entry);
+                this.loadClass(c);
+            } catch (ClassNotFoundException e) {
+
+            }
+        }
+        jar.close();
+    }
+
+    private void loadClass(Class<?> c) {
+        loadOpMode(c);
+        for (Class<?> subclass : c.getDeclaredClasses()) {
+            loadClass(subclass);
+        }
+    }
+
+    private void loadOpMode(Class<?> c) {
+        if (OpMode.class.isAssignableFrom(c) && OpModeClassLoader.isInstantiable(c)) {
+            this.opModeList.add((Class<? extends OpMode>) c);
+        }
+    }
+
+    private static boolean isInstantiable(Class<?> c) {
+        try {
+            c.newInstance();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
     private ClassLoader getClassLoader(List<URL> jarList) {
         String pathString = getDelimitedPathString(jarList);
-        File cacheFile = FileLoader.getCacheDirectory();
-        cacheFile.mkdirs();
-        String cacheDir = cacheFile.toString();
+        String cacheDir = FileLoader.getCacheDirectory().toString();
         ClassLoader parentLoader = this.getClass().getClassLoader();
         return new DexClassLoader(pathString, cacheDir, null, parentLoader);
     }
 
-    private static String getDelimitedPathString(List<? extends Object> list) {
+    private static String getDelimitedPathString(List<?> list) {
         String result = "";
         for (Object o : list) {
             result += File.pathSeparator;
             result += o.toString();
         }
         return result.substring(1);
-    }
-
-
-    // everything below is unorganized
-
-
-    private static void loadJarFile(File jarfile) throws IOException {
-        File cache = new File(FileLoader.getCacheDirectory() + "/temp/");
-        DexFile jarobj = DexFile.loadDex(jarfile.getAbsolutePath(), cache.getAbsolutePath(), 0);
-        Enumeration<String> jarentries = jarobj.entries();
-        while (jarentries.hasMoreElements()) {
-            String entry = jarentries.nextElement();
-            try {
-                Class<?> c = classLoader.loadClass(entry);
-                attemptLoadClass(c);
-            }
-            catch (Throwable ex) {
-
-            }
-        }
-        jarobj.close();
-    }
-
-    private static void attemptLoadClass(Class<?> c) {
-        attemptLoadOpMode(c);
-        for (Class<?> i : c.getDeclaredClasses())
-            attemptLoadClass(i);
-    }
-
-    @SuppressWarnings("unchecked")
-    private static void attemptLoadOpMode(Class<?> c) {
-        try {
-            if (OpMode.class.isAssignableFrom(c) && attemptInstantiate((Class<OpMode>) c)) {
-                ThunderLog.i("Found " + c.getSimpleName() + " as an op mode");
-                opModeList.add((Class<OpMode>) c);
-            }
-        } catch (Throwable ignore) {
-
-        }
-    }
-
-    private static boolean attemptInstantiate(Class<? extends OpMode> c) {
-        try {
-            Object instance = c.newInstance();
-        } catch (IllegalAccessException ex) {
-            return false;
-        } catch (InstantiationException ex) {
-            return false;
-        }
-        return true;
-    }
-
-    private static URL[] getJarURLs(List<File> fileList) {
-        List<URL> jarList = new ArrayList<URL>();
-        for (File f : fileList) {
-            if (f.isFile() && f.getName().endsWith(".jar")) {
-                try {
-                    jarList.add(f.getAbsoluteFile().toURI().toURL());
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return jarList.toArray(new URL[jarList.size()]);
     }
 
 }
